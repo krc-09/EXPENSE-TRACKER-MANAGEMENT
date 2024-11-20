d5f5b266217e242b188c3b5a3b7fbc2c1f77ed7a
krc-09 <kankanarc2020@gmail.com>
2024-11-20 21:00:20 +0530

    did final changes

--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 require('dotenv').config(); 
 const Sequelize = require('sequelize');
@@ -6,13 +5,6 @@ const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,proc
 
     dialect : 'mysql',
     host:process.env.DB_HOST
-=======
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('expense-tracker-app','root','password',{
-
-    dialect : 'mysql',
-    host:'localhost'
->>>>>>> 4ec143124690b802c7905c1975b5df9a06201365
 
 });
 module.exports = sequelize;
